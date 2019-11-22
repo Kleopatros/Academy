2966b1002a622dfa135b7e7b05a107177ad73ec5
Kleopatros <47168369+Kleopatros@users.noreply.github.com>
2019-11-22 14:10:36 -0800

    Add Sandbox.
    Add Nonces directory for one-off solutions and scripts.
    
    Add nonce for parsing exported Google Fit data.

---
+++ src/Academy.Cs/Nonces/N20190901GoogleFit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Academy.Cs.Nonces
+{
+    /// <summary>
+    /// Consumes the daily biometric files of a Google Fit export, extracts the average weight, and
+    /// consolidates all records in a single output file.
+    /// </summary>
+    /// <remarks>
+    /// Relevant columns:
+    ///      [0] Start time
+    ///      [1] End time
+    ///     [12] Average weight (kg)
+    ///     [13] Max weight (kg)
+    ///     [14] Min weight (kg)
+    /// </remarks>
+    [TestClass]
+    public class N20190901GoogleFit
+    {
+        [TestMethod]
+        public void Main()
+        {
+            // Must contain the daily summaries (".\yyyy-MM-dd.csv")
+            const string RootPath = @"";
+
+            string[] files = Directory.GetFiles(RootPath, "*.csv");
+            foreach (string file in files.Where(x => Regex.IsMatch(x, @"\d{4}-\d{2}-\d{2}\.csv")))
+            {
+                // Record date is the file name, sans extension
+                string date = file.Split('\\').Last().Split('.')[0];
+                using (StreamReader stream = File.OpenText(file))
+                {
+                    string line = stream.ReadLine();
+                    string header = line;
+                    while ((line = stream.ReadLine()) != null)
+                    {
+                        string[] columns = line.Split(',');
+                        string timeAsString = columns[0]; // "Start time"
+                        string valueAsString = columns[12]; // "Average weight (kg)"
+
+                        if (!string.IsNullOrWhiteSpace(valueAsString))
+                        {
+                            // Start time column excludes the date, includes an invalid suffix, and is in the incorrect timezone
+                            DateTime timestamp = DateTime.Parse($"{date} {timeAsString.Substring(0, 5)}").AddHours(-3);
+                            string timestampAsString = timestamp.ToString("yyyy-MM-dd HH:mm");
+
+                            // Convert to lbs
+                            double value = double.Parse(valueAsString) * 2.20462;
+
+                            File.AppendAllText(Path.Combine(RootPath, "results.csv"), $"{timestampAsString},{value}\r\n");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Academy.Cs/Sandbox.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Academy.Cs
+{
+    /// <summary>
+    /// Permanently empty class for use as a quick sandbox or scratch-pad.
+    /// </summary>
+    [TestClass]
+    public class Sandbox
+    {
+        [TestMethod]
+        public void Main()
+        {
+            // Scratch code here
+        }
+    }
+}
